3580ed57de4673ad52c241ffc2fb505aa7bd96d8
Prepsld <119632733+Prepsld@users.noreply.github.com>
2023-11-03 13:06:40 -0600

    Initializes website

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,22 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        prowling: {
+          "primary": "#43BADB",
+          "secondary": "#F92C90",
+          "accent": "#9C6B9E",
+          "neutral": "#322833",
+          "base-100": "#3C3242",
+          "info": "#4FCFF2",
+          "success": "#F7E031",
+          "warning": "#e1a60e",
+          "error": "#e94e4e",
+        },
+    },
+    ],
+  },
+  plugins: [require("daisyui")],
 }
