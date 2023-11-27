56db770b50917a19e90bea92fbb77c2fa4ce97b0
Prepsld <119632733+Prepsld@users.noreply.github.com>
2023-11-27 07:26:19 -0700

    Fixes blurb

--- app/components/Blurb.js
+++ app/components/Blurb.js
@@ -6,7 +6,7 @@ const Blurb = () => {
     <section class="prose" className="container mx-auto mt-8">
       <h2 className="text-3xl font-bold mb-4">Who Am I?</h2>
       <p className="text-gray-600">
-        Your short blurb or description goes here. Highlight your skills, interests, and what you're passionate about.
+        Your short blurb or description goes here. Highlight your skills, interests, and what you are passionate about.
       </p>
     </section>
   );
