b27d52e6d3f586832571fd5a0eadb1ffc9378ea2
Prepsld <119632733+Prepsld@users.noreply.github.com>
2023-12-04 09:14:22 -0700

    Updates Route.js
    I can actually make accounts again.

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -8,54 +8,53 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 
  const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    CredentialsProvider({
-      id: "credentials",
-      name: "MongoDB Credentials",
-      credentials: {
-        username: { label: "Username", type: "text ", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        const client = await clientPromise; // Get the MongoDB client
-        const db = client.db(user);
+   // Configure one or more authentication providers
+   providers: [
+     GoogleProvider({
+       clientId: process.env.GOOGLE_CLIENT_ID,
+       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+     }),
+     GitHubProvider({
+       clientId: process.env.GITHUB_ID,
+       clientSecret: process.env.GITHUB_SECRET,
+     }),
+     CredentialsProvider({
+       id: "credentials",
+       name: "MongoDB Credentials",
+       credentials: {
+         username: { label: "Username", type: "text ", placeholder: "jsmith" },
+         password: { label: "Password", type: "password" },
+       },
+       async authorize(credentials) {
+         const client = await clientPromise; // Get the MongoDB client
+         const db = client.db(process.env.MONGODB_DB);
 
-        const existingUser = await db
-          .collection("users")
-          .findOne({ username: credentials.username });
+         const existingUser = await db
+           .collection("users")
+           .findOne({ username: credentials.username });
 
-        if (existingUser) {
-          if (existingUser.password === credentials.password) {
-            // If the passwords match, return the user
-            return existingUser;
-          } else {
-            // If the passwords don't match, throw an error
-            throw new Error("Incorrect password");
-          }
-        }
+         if (existingUser) {
+           if (existingUser.password === credentials.password) {
+             // If the passwords match, return the user
+             return existingUser;
+           } else {
+             // If the passwords don't match, throw an error
+             throw new Error("Incorrect password");
+           }
+         }
 
-        const newUser = await db.collection("users").insertOne({
-          username: credentials.username,
-          password: credentials.password,
-        });
+         const newUser = await db.collection("users").insertOne({
+           username: credentials.username,
+           password: credentials.password,
+         });
 
-        return newUser;
-      },
-      
-    }),
-    // ...add more providers here
-  ],
-  adapter: MongoDBAdapter(clientPromise),
-};
+         return newUser;
+       },
+     }),
+     // ...add more providers here
+   ],
+   adapter: MongoDBAdapter(clientPromise),
+ };
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
