0f48ba62479e1541725a2abf862a48544c169ee6
Prepsld <119632733+Prepsld@users.noreply.github.com>
2023-12-14 11:09:33 -0700

    Makes navigation "mobile" friendly

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -17,53 +17,110 @@ const Navigation = () => {
 
 
   return (
-    <nav className=" navbar bg-base-300">
-      <div className="container mx-auto flex items-center justify-between space-x-1">
-        <div>
-          <Link
-            href="/"
-            className="btn btn-ghost btn-xs sm:btn-sm md:btn-md lg:btn-lg text-md"
-          >
-            Home Page
-          </Link>
-          <Link
-            href="/documentation"
-            className="btn btn-ghost btn-xs sm:btn-sm md:btn-md lg:btn-lg text-md"
-          >
-            Documentation
-          </Link>
-          <Link
-            href="/projects"
-            className="btn btn-ghost btn-xs sm:btn-sm md:btn-md lg:btn-lg text-md"
-          >
-            Projects
-          </Link>
-          <Link
-            href="/resume"
-            className="btn btn-ghost btn-xs sm:btn-sm md:btn-md lg:btn-lg text-md"
-          >
-            Resume
-          </Link>
-          <Link
-            href="/contact"
-            className="btn btn-ghost btn-xs sm:btn-sm md:btn-md lg:btn-lg text-md"
-          >
-            Contact Me
-          </Link>
-          {/* Add more links as needed */}
+    <div className="drawer ">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        {/* Navbar */}
+        <div className="w-full navbar bg-base-300">
+          <div className="flex-none lg:hidden">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-6 h-6 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <div className="flex-none hidden lg:block max-w-screen-md">
+            <ul className="menu menu-horizontal ">
+              {/* Navbar menu content here */}
+              <li>
+                <Link href="/">Home Page</Link>
+              </li>
+              <li>
+                <Link href="/documentation">Documentation</Link>
+              </li>
+              <li>
+                <Link href="/projects">Projects</Link>
+              </li>
+              <li>
+                <Link href="/resume">Resume</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact Me</Link>
+                {/* Add more links as needed */}
+              </li>
+
+              <select
+                className="select select-secondary p-1 ml-1"
+                data-choose-theme
+              >
+                <option disabled value="">
+                  Pick a theme
+                </option>
+                <option value="cupcake">Light</option>
+                <option value="business">Dark</option>
+                <option value="retro">Retro</option>
+              </select>
+              <SignOutButton />
+            </ul>
+          </div>
         </div>
-        <SignOutButton />
       </div>
-      <select className="select select-secondary" data-choose-theme>
-        <option disabled value="">
-          Pick a theme
-        </option>
-        <option value="cupcake">Light</option>
-        <option value="business">Dark</option>
-        <option value="retro">Retro</option>
-      </select>
-    </nav>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-3"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu p-4 w-80 min-h-full bg-base-200">
+          {/* Sidebar content here */}
+          <li>
+            <Link href="/">Home Page</Link>
+          </li>
+          <li>
+            <Link href="/documentation">Documentation</Link>
+          </li>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/resume">Resume</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact Me</Link>
+            {/* Add more links as needed */}
+          </li>
+
+          <SignOutButton />
+          <select className="select select-secondary mt-1" data-choose-theme>
+            <option disabled value="">
+              Pick a theme
+            </option>
+            <option value="cupcake">Light</option>
+            <option value="business">Dark</option>
+            <option value="retro">Retro</option>
+          </select>
+        </ul>
+      </div>
+    </div>
   );
 };
 
 export default Navigation;
+
+
+
+
